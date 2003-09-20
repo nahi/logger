7b1c1f874b43f4a91530e8d851577bad03d0811d
nahi <nahi@16c69c39-ecea-0310-a561-fa2f022cc31d>
2003-09-20 14:49:21 +0000

    Add compatible interface.

--- lib/devel/logger.rb
+++ lib/devel/logger.rb
@@ -13,9 +13,20 @@ module Devel
       SEV_UNKNOWN = 6
     end
     include Severity
+    alias sevThreshold level
+    alias sevThreshold= level=
   end
   LogDevice = ::Logger::LogDevice
   class Application < ::Logger::Application
     include Devel::Logger::Severity
+    def initialize(*arg)
+      super
+      setLog(STDERR)
+    end
+    def setLog(logdev, shift_age = 0, shift_size = 102400)
+      @log = Devel::Logger.new(logdev, shift_age, shift_size)
+      @log.progname = @appname
+      @log.level = @level
+    end
   end
 end
